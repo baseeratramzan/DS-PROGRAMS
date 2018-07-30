70d86f20b070dec08bf0381404d95166a4edca8c
baseerat <41920673+baseerat@users.noreply.github.com>
2018-07-31 01:04:04 +0530

    Arrays

---
+++ traversal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//Write a Program which perform traversing operation.
+#include <stdio.h>
+void main()
+{
+    int LA[] = {2,4,6,8,9};
+    int i, n = 5;
+    
+    printf("The array elements are:\n");
+    
+    for(i = 0; i < n; i++)
+    {
+        printf("LA[%d] = %d \n", i, LA[i]);
+    }
+}
